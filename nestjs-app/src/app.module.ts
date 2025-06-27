d0596ca6a08b23563a2c591fb7729d17f033c046
zyadelhady <zyade40@gmail.com>
2025-06-27 22:21:53 +0300

    added pipe validation and user schema

--- nestjs-app/src/app.module.ts
+++ nestjs-app/src/app.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      process.env.MONGODB_URI || 'mongodb://localhost:27017/nest_db',
-    ),
-  ],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URI as string)],
   controllers: [AppController],
   providers: [AppService],
 })
