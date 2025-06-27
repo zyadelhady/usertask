719e01daf67ee93144f4b666eb1fff334d59e6d7
zyadelhady <zyade40@gmail.com>
2025-06-27 22:03:33 +0300

    init with docker

---
+++ nestjs-app/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI || 'mongodb://localhost:27017/nest_db',
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
