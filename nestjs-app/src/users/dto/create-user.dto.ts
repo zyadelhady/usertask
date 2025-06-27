b47365d126110264dbf7c2bed2045322fa17cb75
zyadelhady <zyade40@gmail.com>
2025-06-27 22:23:57 +0300

    added user dtos

---
+++ nestjs-app/src/users/dto/create-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  age: number;
+}
